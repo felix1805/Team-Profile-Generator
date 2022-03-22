6a0ff11ab24f6fa8880863fafe762a46bb91248b
felix1805 <felix1805@gmail.com>
2022-03-21 22:08:11 -0400

    started constructing prompt menues and generating individual .js files for employees

--- index.js
+++ index.js
@@ -1,81 +1,138 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const members = [];
+const jest = require('jest');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern')
+const teamInfo = [];
 
-const promptManager = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            message: 'What is your first name? (Required Input)',
-            name: 'name',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('You did not enter a Name!')
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            message: 'What is your employee ID? (Required Input',
-            name: 'employeeId',
-            validate: employeeId => {
-                if (employeeId) {
-                    return true;
-                } else {
-                    console.log('You did not enter your Employee Id!')
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            message: 'What is your email address? (Required Input)',
-            name: 'email',
-            validate: (email) => {
-                if (email) {
-                    return true;
-                } else {
-                    console.log('You did not enter your email address!')
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            message: 'What is your office number (Required Input)',
-            name: 'officeNumber',
-            validate: (officeNumber) => {
-                if (officeNumber) {
-                    return true;
-                } else {
-                    console.log('You did not enter your office number!')
-                    return false;
-                }
-            }
-        },
-    ]).then(answers => {
-        console.log(answers);
-        const manager = new Manager(answers.name, answers.employeeId, ansers.email, answers.officeNumber);
-        members.push(manager);
-        promptSelections();
 
-    })
+const managerPrompt = () => {
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is your team manager\'s first name?',
+                name: 'managerName',
+            },
+            {
+                type: 'input',
+                message: 'What is your team manager\'s employee ID?',
+                name: 'managerId',
+
+            },
+            {
+                type: 'input',
+                message: 'What is your manager\'s email address?',
+                name: 'managerEmail',
+
+            },
+            {
+                type: 'input',
+                message: 'What is your manager\'s office number?',
+                name: 'managerOffice'
+
+            }])
+        .then(response => {
+            const manager = new Manager(response.managerName, response.managerId, response.managerEmail, response.manageroffice)
+            teamInfo.push(manager);
+            console.log(manager);
+            promptMain();
+        })
 };
 
-const promptSelections = () => {
-    return inquirer.prompt([
-        {
-            type: 'list',
-            message: 'what type of employee would you like to add next?'
-            choices: ['Engineer', 'Intern', 'Complete team as-is']
-        }
-    ])
-}
+const promptMain = () => {
+    return inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'Are you done building your team? Or do you have to add more users?',
+                name: 'finishTeam',
+                choices: [
+                    { name: 'Add an Engineer', value: 'add engineer' },
+                    { name: 'Add an Intern', value: 'add intern' },
+                    { name: 'Finish building my Team', value: 'finish team' },
+                ]
+            }])
+        .then(userSelection => {
+            switch (userSelection.menu) {
+                case "Add an Engineer":
+                    engineerPrompt();
+                    break;
+                case "Add an Intern":
+                    internPrompt();
+                    break;
+                case "finish building team":
+                    assembleTeam();
+                    break;
 
+            }
 
+        });
+};
 
+const engineerPrompt = () => {
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the Engineer\'s name?',
+                name: 'engineerName'
+            },
+            {
+                type: 'input',
+                message: 'What is the engineer\'s employee ID?',
+                name: 'engineerID'
+            },
+            {
+                type: 'input',
+                message: 'What is the engineer\'s email?',
+                name: 'engineerEmail'
+            },
+            {
+                type: 'value',
+                message: 'What is the engineer\'s gitHub username?',
+                name: 'engineerGitHub'
+            }])
+        .then(response => {
+            const engineer = new Engineer(response.engineerName, response.engineerID, response.engineerEmail, response.engineerGitHub);
+            teamInfo.push(engineer);
+            console.log(engineer)
+            promptMain();
+        })
+};
+const internPrompt = () => {
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the Intern\'s name?',
+                name: 'internName'
+            },
+            {
+                type: 'input',
+                message: 'What is the Inter\'s employee ID?',
+                name: 'internID'
+            },
+            {
+                type: 'input',
+                message: 'What is the Intern\'s email?',
+                name: 'internEmail'
+            },
+            {
+                type: 'input',
+                message: 'What is the Intern\'s School username?',
+                name: 'internSchool'
+            }])
+        .then(response => {
+            const intern = new Intern(response.internName, response.internID, response.internEmail, response.internSchool);
+            teamInfo.push(intern);
+            console.log(intern);
+            promptMain();
+        })
+};
+const assembleTeam = () => {
 
+};
 
+managerPrompt();
